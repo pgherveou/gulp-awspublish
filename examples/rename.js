2159f7a6c1ba109947eea7b50dab2e805fa1f1cc
PG <pgherveou@gmail.com>
2014-03-26 10:56:00 -0700

    add examples

---
+++ examples/rename.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var rename = require('gulp-rename');
+var awspublish = require('../');
+
+var credentials = JSON.parse(fs.readFileSync('aws-credentials.json', 'utf8'));
+var publisher = awspublish.create(credentials);
+
+
+gulp.src('examples/fixtures/*.js')
+    .pipe(rename(function (path) {
+        path.dirname += '/s3-examples';
+        path.basename += '-s3';
+    }))
+    .pipe(publisher.publish())
+    .pipe(awspublish.reporter());
