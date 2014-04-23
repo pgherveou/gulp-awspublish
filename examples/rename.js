640af1c8f009f75e9695339e56a47d60a5a94cc1
PG <pgherveou@gmail.com>
2014-04-22 23:49:58 -0700

    Fixes #45

--- examples/rename.js
+++ examples/rename.js
@@ -8,10 +8,23 @@ var credentials = JSON.parse(fs.readFileSync('aws-credentials.json', 'utf8'));
 var publisher = awspublish.create(credentials);
 
 
-gulp.src('examples/fixtures/*.js')
-    .pipe(rename(function (path) {
-        path.dirname += '/s3-examples';
-        path.basename += '-s3';
-    }))
-    .pipe(publisher.publish())
-    .pipe(awspublish.reporter());
+// gulp.src('examples/fixtures/*.js')
+//   .pipe(rename(function (path) {
+//     path.dirname += '/s3-examples';
+//     path.basename += '-s3';
+//   }))
+//   .pipe(publisher.publish())
+//   .pipe(awspublish.reporter());
+
+
+
+// gulp.src('**/*.js', {cwd: 'examples'})
+//   .pipe(publisher.publish())
+//   .pipe(awspublish.reporter());
+
+gulp.src('public/assets/**', {cwd: 'examples'})
+  .pipe(rename(function (path) {
+    path.dirname = 'public/assets/' + path.dirname;
+  }))
+  .pipe(publisher.publish())
+  .pipe(awspublish.reporter());
